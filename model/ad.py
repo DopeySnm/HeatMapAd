72eaedbf3a7bea38fe7fca438f28eb1cbf0e2403
Artur <fartur09@mail.ru>
2023-07-02 00:44:05 +0500

    create dbHelper

---
+++ model/ad.py
@@ -0,0 +1,33 @@
+from datetime import date
+
+from db.base import Base
+from model.description import Description
+from model.location import Location
+from model.real_estate import RealEstate
+from sqlalchemy import Column, Integer, String,  ForeignKey, Float, Date
+
+class Ad(RealEstate, Base):
+    __tablename__ = 'ad'
+
+    id = Column(Integer, primary_key=True)
+    price = Column(Float)
+    linc = Column(String)
+    title = Column(String)
+    magnitude = Column(Float)
+    data_download = Column(Date)
+    location = Column(Integer, ForeignKey('location.id'))
+    description = Column(Integer, ForeignKey('description.id'))
+
+    def __init__(self,
+                 price: float,
+                 linc: str,
+                 title: str,
+                 location: Location,
+                 magnitude: float,
+                 data_download: date,
+                 description: Description = None):
+        super().__init__(title, location, data_download)
+        self.price = price
+        self.linc = linc
+        self.magnitude = magnitude
+        self.description = description
---
+++ model/real_estate.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+from model.location import Location
+
+class RealEstate:
+    def __init__(self,
+                title: str,
+                location: Location,
+                data_download: date):
+        self.title = title
+        self.location = location
+        self.data_download = data_download
